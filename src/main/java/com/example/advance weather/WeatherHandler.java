150867df479a6d20b29c1cb83cdcaeb6eff09d5d
jimj90201-a11y <jimj90201@gmail.com>
2026-06-29 22:59:55 +0530

    Add WeatherHandler class with weather constants

---
+++ src/main/java/com/example/advance weather/WeatherHandler.java
@@ -0,0 +1,11 @@
+package com.gamerofpro.advanceweather;
+
+public class WeatherHandler {
+
+    public static final int CHECK_INTERVAL_TICKS = 20 * 60 * 10;
+    public static final int WEATHER_DURATION_TICKS = 20 * 60 * 5;
+
+    public static final double RAIN_CHANCE = 0.35;
+    public static final double THUNDER_CHANCE = 0.15;
+
+}
