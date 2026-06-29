9515b3f9dd60d70b55966bb98c516c97029163be
jimj90201-a11y <jimj90201@gmail.com>
2026-06-29 23:27:26 +0530

    Reorder imports and register event handler in constructor

--- src/main/java/com/gamerofpro/advanceweather/AdvanceWeather.java
+++ src/main/java/com/gamerofpro/advanceweather/AdvanceWeather.java
@@ -1,11 +1,7 @@
 package com.gamerofpro.advanceweather;
 
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.common.MinecraftForge;
-
-public AdvanceWeather() {
-    MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
-}
+import net.minecraftforge.fml.common.Mod;
 
 @Mod(AdvanceWeather.MOD_ID)
 public class AdvanceWeather {
@@ -13,5 +9,6 @@ public class AdvanceWeather {
     public static final String MOD_ID = "advanceweather";
 
     public AdvanceWeather() {
+        MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
     }
 }
