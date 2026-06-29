fcd045ecc8677f77a28ff09d2b400eac3ed8fa26
jimj90201-a11y <jimj90201@gmail.com>
2026-06-29 22:59:22 +0530

    Create AdvanceWeather.java

---
+++ src/main/java/com/example/advance weather/AdvanceWeather.java
@@ -0,0 +1,12 @@
+package com.gamerofpro.advanceweather;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdvanceWeather.MOD_ID)
+public class AdvanceWeather {
+
+    public static final String MOD_ID = "advanceweather";
+
+    public AdvanceWeather() {
+    }
+}
