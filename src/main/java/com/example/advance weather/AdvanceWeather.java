81d0c48c1e5b82ec758b6a322930a289bc186849
jimj90201-a11y <jimj90201@gmail.com>
2026-06-29 23:01:50 +0530

    Register ForgeEventHandler on initialization
    Register ForgeEventHandler with the Minecraft Forge event bus.

--- src/main/java/com/example/advance weather/AdvanceWeather.java
+++ src/main/java/com/example/advance weather/AdvanceWeather.java
@@ -1,6 +1,11 @@
 package com.gamerofpro.advanceweather;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.common.MinecraftForge;
+
+public AdvanceWeather() {
+    MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
+}
 
 @Mod(AdvanceWeather.MOD_ID)
 public class AdvanceWeather {
